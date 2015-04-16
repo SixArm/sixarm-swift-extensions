1ffbfe165b98f481abda835ce78e7da72291dd42
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-04-16 15:54:53 -0400

    Capitalize Extensions

--- extensions/String/String_indexOf.swift
+++ extensions/String/String_indexOf.swift
@@ -2,21 +2,13 @@ extension String {
 
   func indexOf(target: String) -> Int {
     var range = self.rangeOfString(target)
-    if let range = range {
-      return distance(self.startIndex, range.startIndex)
-    } else {
-      return -1
-    }
+    return range != nil ? distance(self.startIndex, range.startIndex) : -1
   }
 
   func indexOf(target: String, startIndex: Int) -> Int {
     var startRange = advance(self.startIndex, startIndex)        
     var range = self.rangeOfString(target, options: NSStringCompareOptions.LiteralSearch, range: Range<String.Index>(start: startRange, end: self.endIndex))
-    if let range = range {
-      return distance(self.startIndex, range.startIndex)
-    } else {
-      return -1
-    }
+    return (range != nil) ? distance(self.startIndex, range.startIndex) : -1
   }
 
 }
